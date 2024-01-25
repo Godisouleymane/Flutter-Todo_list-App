14ce8c9b66742dd6d88d1d767ea1878427c3f3eb
GodiSouleymane <ssabiougodi@gmail.com>
2024-01-25 22:37:44 +0000

    AppBar design

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,64 +8,66 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Todo List',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Colors.white,
+        title: ListTile(
+          leading: Padding(
+            padding: const EdgeInsets.only(bottom: 5.0),
+            child: ClipOval(
+              child: Image.asset(
+                height: 60,
+                width: 60,
+                'assets/man.jpg',
+               fit: BoxFit.cover,),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          title: Text('Hello, I\'m', 
+          style: TextStyle(
+            color: Colors.grey.shade400
+          ),),
+          subtitle: const Text('Souleymane', 
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold
+          ),),
         ),
+        actions: [
+          Padding(padding: EdgeInsets.symmetric(horizontal: 12.0),
+            child: Row(
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.calendar)),
+                IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bell)),
+                
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
